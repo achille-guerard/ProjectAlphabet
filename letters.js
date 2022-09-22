29b5d00cce64f1661d4d2c43a6a000b2fde58b9b
achille_guerard <achille.guerard@epita.fr>
2022-09-22 19:35:38 +0200

    done bitch the css is shit but who cares

--- letters.js
+++ letters.js
@@ -1,20 +1,26 @@
 var arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var length = 26;
 
+
 function main() {
-    while (length != 0 /*&& button is pushed*/ ) {
-        letter(arr, length);
+    letter();
+    curr();
+}
+
+function letter() {
+    if (length == 0)
+        var chosen = "No more letter bitch"
+    else {
+        var random_i = Math.floor(Math.random() * length);
+        var chosen = arr[random_i];
+        [arr[random_i], arr[length - 1]] = [arr[length - 1], arr[random_i]];
+        arr.pop();
         length--;
+
     }
-
+    document.getElementById("letter_display").innerHTML = chosen;
 }
 
-function letter(arr, length) {
-    var random_i = Math.floor(Math.random() * length);
-    var chosen = arr[random_i];
-    [arr[random_i], arr[length - 1]] = [arr[length - 1], arr[random_i]];
-    arr.pop();
-    console.log(chosen);
+function curr() {
+    document.getElementById("cur").innerHTML = "Current letter :";
 }
-
-main();
